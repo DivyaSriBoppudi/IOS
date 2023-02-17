2532a5f74248d0ce25ecdac9dad57b28ad9cf59d
Divya sri <112046045+DivyaSriBoppudi@users.noreply.github.com>
2023-02-17 16:29:27 -0600

    ArthmaticUI

---
+++ ArthmaticUI/ArithmaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArthmaticUI
+//
+//  Created by Boppudi,Divya Sri on 2/2/23.
+//
+
+import UIKit
+
+class ArithmaticVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
