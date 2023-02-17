ff8164493df5d77d26404a799b9d68e3ee17d3d9
Divya sri <112046045+DivyaSriBoppudi@users.noreply.github.com>
2023-02-17 16:30:13 -0600

    Add files via upload

---
+++ ImageDemo/ImageDemo/CoursesVC.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ImageDemo
+//
+//  Created by Boppudi,Divya Sri on 2/14/23.
+//
+
+import UIKit
+
+class CoursesVC: UIViewController {
+    
+    //App Model
+    
+    let Courses: Dictionary<String,Dictionary<String,String>> = [
+        :]
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
